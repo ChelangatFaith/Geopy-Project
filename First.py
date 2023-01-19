87f11b2830ed6009013fb7738b7039f19b00e1b2
SamanthaFaith <115935667+ChelangatFaith@users.noreply.github.com>
2023-01-19 12:54:58 +0300

    Add files via upload

---
+++ First.py
@@ -0,0 +1,13 @@
+
+#First python project
+print("Hello world")
+
+#Getting the input from the user
+Name = input("Tell us something")
+#Prints the input from the user
+print (Name)
+
+#comments are unexecutable in a way that they are not executed
+#They explain more about your code
+#comments we use ## or''' to show
+#comments makes one understand your project
